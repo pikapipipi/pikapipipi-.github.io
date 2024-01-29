73f2c39c8ee92c89867de7293710a28d3ef48f94
pikapipipi <154891457+pikapipipi@users.noreply.github.com>
2024-01-29 16:27:44 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,75 +1,85 @@
-const dataArr= [
-    {
-      gallery:"freecodecamp",
-      ahref: "./pokemonsearch/pokedex.html",
-      aalt:"pokedex",
-      imgsrc: "./image/pkm_search.png",
-      title: "Pokemon Search",
-      caption: "Learning how to fetch data from an external API.",
-	uploaddate: 20231223,
-    },
-    {
-      gallery:"freecodecamp",
-      ahref: "./palindromechecker/palindromechecker.html",
-      aalt:"palindrome",
-      imgsrc: "./image/palindrome_checker.png",
-      title: "Palindrome Checker",
-      caption: "Learning how to validate user input",
-	uploaddate: 20231225},
-    {
-      gallery:"freecodecamp",
-      ahref: "./romannumeral/romannumeral.html",
-      aalt:"roman numeral",
-      imgsrc: "./image/romannumeral.png",
-      title: "Roman Numeral Converter",
-      caption: "Learning how to perform calculations based on user input.",
-	uploaddate: 20231227,
-    },
-	{
-      gallery:"freecodecamp",
-      ahref: "index.html#homework",
-      aalt:"webpage",
-      imgsrc: "./image/webpage.png",
-      title: "This Webpage",
-      caption: "Learning how to work with DOM manipulation, object destructuring and data filtering.",
-	uploaddate: 20231228,
-	},
-   	{
-      gallery:"freecodecamp",
-      ahref: "./cashregister/cashregister.html",
-      aalt:"cash register",
-      imgsrc: "./image/cashregister.png",
-      title: "Cash Register",
-      caption: "Learning how to work with floating point calculations.",
-	uploaddate: 20240120,
-	},
-    {
-      gallery:"gsuite",
-      ahref: "https://docs.google.com/spreadsheets/d/1jAxTbayo7MX5IASvxjHtZvSFqblyEwvYhjz6c1ZzULA/edit?usp=sharing",
-      aalt:"Google Sheets",
-      imgsrc: "./image/chemical_search.png",
-      title: "Sheets for Searching Inventory",
-      caption: "Aim to find any items in a second. Delighted it's still looked after by my old labmates.",
-	uploaddate: 20220830,
-    },
-    {
-      gallery:"gsuite",
-      ahref: "https://sites.google.com/view/demoschedule/",
-      aalt:"Apps Script",
-      imgsrc: "./image/googlecalendar.png",
-      title: "Google Sheets Apps Script",
-      caption: "Batch upload list of events from spreadsheet to calendar.",
-	uploaddate: 20220829,
-    },
-    {
-      gallery:"gsuite",
-      ahref: "https://sites.google.com/view/labtools",
-      aalt:"Google Site",
-      imgsrc: "./image/googlesite.png",
-      title: "Google's Site",
-      caption: 'A prototype for my labmates, and later handed to my colleagues.<a target="_blank" href="https://sites.google.com/study.hkmu.edu.hk/labtools/" alt="Google Site"><u>Click here for their site.</u></a>',
-	  uploaddate: 20220828,
-    }
+const dataArr = [
+  {
+    gallery: "freecodecamp",
+    ahref: "https://pikapipipi.github.io/randomquote",
+    aalt: "random quote",
+    imgsrc: "./image/randomquote.png",
+    title: "Random Quotes",
+    caption: "Learning how to work with React library with conditional rendering and short circuit evaluation",
+    uploaddate: 20240129,
+  },
+  {
+    gallery: "freecodecamp",
+    ahref: "./cashregister/cashregister.html",
+    aalt: "cash register",
+    imgsrc: "./image/cashregister.png",
+    title: "Cash Register",
+    caption: "Learning how to work with floating point calculations.",
+    uploaddate: 20240120,
+  },
+  {
+    gallery: "freecodecamp",
+    ahref: "index.html#homework",
+    aalt: "webpage",
+    imgsrc: "./image/webpage.png",
+    title: "This Webpage",
+    caption: "Learning how to work with DOM manipulation, object destructuring and data filtering.",
+    uploaddate: 20231228,
+  },
+  {
+    gallery: "freecodecamp",
+    ahref: "./romannumeral/romannumeral.html",
+    aalt: "roman numeral",
+    imgsrc: "./image/romannumeral.png",
+    title: "Roman Numeral Converter",
+    caption: "Learning how to perform calculations based on user input.",
+    uploaddate: 20231227,
+  },
+  {
+    gallery: "freecodecamp",
+    ahref: "./palindromechecker/palindromechecker.html",
+    aalt: "palindrome",
+    imgsrc: "./image/palindrome_checker.png",
+    title: "Palindrome Checker",
+    caption: "Learning how to validate user input",
+    uploaddate: 20231225,
+  },
+  {
+    gallery: "freecodecamp",
+    ahref: "./pokemonsearch/pokedex.html",
+    aalt: "pokedex",
+    imgsrc: "./image/pkm_search.png",
+    title: "Pokemon Search",
+    caption: "Learning how to fetch data from an external API.",
+    uploaddate: 20231223,
+  },
+  {
+    gallery: "gsuite",
+    ahref: "https://docs.google.com/spreadsheets/d/1jAxTbayo7MX5IASvxjHtZvSFqblyEwvYhjz6c1ZzULA/edit?usp=sharing",
+    aalt: "Google Sheets",
+    imgsrc: "./image/chemical_search.png",
+    title: "Sheets for Searching Inventory",
+    caption: "Aim to find any items in a second. Delighted it's still looked after by my old labmates.",
+    uploaddate: 20220830,
+  },
+  {
+    gallery: "gsuite",
+    ahref: "https://sites.google.com/view/demoschedule/",
+    aalt: "Apps Script",
+    imgsrc: "./image/googlecalendar.png",
+    title: "Google Sheets Apps Script",
+    caption: "Batch upload list of events from spreadsheet to calendar.",
+    uploaddate: 20220829,
+  },
+  {
+    gallery: "gsuite",
+    ahref: "https://sites.google.com/view/labtools",
+    aalt: "Google Site",
+    imgsrc: "./image/googlesite.png",
+    title: "Google's Site",
+    caption: 'A prototype for my labmates, and later handed to my colleagues.<a target="_blank" href="https://sites.google.com/study.hkmu.edu.hk/labtools/" alt="Google Site"><u>Click here for their site.</u></a>',
+    uploaddate: 20220828,
+  }
 
 ];
 dataArr.sort((a, b) => a.uploaddate < b.uploaddate ? 1 : -1); // latest update display first
@@ -79,9 +89,9 @@ const freecodecampHTML = document.getElementById("freecodecampgallery");
 const gsuiteHTML = document.getElementById("gsuitegallery");
 
 const setGalleryhw = (arr = dataArr, innerHTMLbyID) => {
-	  let galleryhwHTML = arr.map(
-      ({ gallery, ahref, aalt, imgsrc, title, caption}) =>
-        `<div class="galleryhw">
+  let galleryhwHTML = arr.map(
+    ({ gallery, ahref, aalt, imgsrc, title, caption }) =>
+      `<div class="galleryhw">
 			<a href="${ahref}" alt="${aalt}"${gallery === 'gsuite' ? ' target="_blank"' : ''}>
 				<img src="${imgsrc}"></a><br/>
 					<span class="gallerytitle">
@@ -89,16 +99,16 @@ const setGalleryhw = (arr = dataArr, innerHTMLbyID) => {
 					</span>
 				<span class="gallerycaption">${caption}</span>
 		</div>`
-    ).join("");
+  ).join("");
 
-	innerHTMLbyID.insertAdjacentHTML('beforeend', galleryhwHTML);
+  innerHTMLbyID.insertAdjacentHTML('beforeend', galleryhwHTML);
 };
 
-      setGalleryhw(
-        dataArr.filter((dataArr) => dataArr.gallery === "freecodecamp")
-      , freecodecampHTML);
+setGalleryhw(
+  dataArr.filter((dataArr) => dataArr.gallery === "freecodecamp")
+  , freecodecampHTML);
 
-      setGalleryhw(
-        dataArr.filter((dataArr) => dataArr.gallery === "gsuite")
-      , gsuiteHTML);
+setGalleryhw(
+  dataArr.filter((dataArr) => dataArr.gallery === "gsuite")
+  , gsuiteHTML);
 
